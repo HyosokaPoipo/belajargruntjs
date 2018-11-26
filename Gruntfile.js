87c20a4b29ae6142b512f86d7467c701ab904564
maryadi <maryadi@hellobaby-project.club>
2018-11-26 10:29:58 +0700

    success compiling with empty css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,29 @@
 module.exports = function(grunt) {
-    
+    const sass = require('node-sass');
+    // require('load-grunt-tasks')(grunt);
     // Configuration
     grunt.initConfig({
         // pass in options to plugins
         // refenrece to files etc
+        sass: {
+            options: {
+                implementation: sass
+            },
+            dist: {
+                files: [{
+                    'css/main.css': 'sass/*.scss'
+                }]
+            }
+        }
     });
 
     
     // Load plugins
-    grunt.loadNpmTasks('');
+    grunt.loadNpmTasks('grunt-sass');
 
     
     // Register tasks
+    grunt.registerTask('convert-sass', 'sass');
     grunt.registerTask('run', function() {
         console.log('running task from grunt');
     });
